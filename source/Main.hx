5e81b33518430235e6a30fd18fcd8c98d77580a2
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-10-14 20:50:50 +0700

    is crash

--- source/Main.hx
+++ source/Main.hx
@@ -2,12 +2,15 @@ package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
+import sys.io.File;
 
 class Main extends Sprite
 {
+	public static var mainFolder:String = File.getContent("system/configFolder.txt");
+
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		addChild(new FlxGame(0, 0, PlayState, 60, 60, true));
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,6 +4,10 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
+import scriptsCode.LuaCode;
+import sys.FileSystem;
+
+using StringTools;
 
 class PlayState extends FlxState
 {
@@ -24,15 +28,44 @@ class PlayState extends FlxState
 	public var images:Map<String, FlxSprite> = new Map<String, FlxSprite>();
 	public var text:Map<String, FlxText> = new Map<String, FlxText>();
 
+	static public var luaScripts:Array<LuaCode> = [];
+
 	override public function create()
 	{
 		super.create();
+		var folders = 'mods/' + Main.mainFolder + 'data/';
+		var folderToRead = FileSystem.readDirectory(folders);
+		for (file in folderToRead)
+		{
+			if (file.endsWith(".lua"))
+			{
+				luaScripts.push(new LuaCode(folders + file));
+			}
+		}
+
+		callOnScripts("onCreate", []);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		callOnScripts("onUpdate", [elapsed]);
 		if (FlxG.keys.justPressed.F12)
 			openSubState(new SubLogState());
+	}
+	private function callOnScripts(funcName:String, args:Array<Dynamic>):Dynamic
+	{
+		var value:Dynamic = LuaCode.Function_Continue;
+
+		for (i in 0...luaScripts.length)
+		{
+			var ret:Dynamic = luaScripts[i].call(funcName, args);
+			if (ret != LuaCode.Function_Continue)
+			{
+				value = ret;
+			}
+		}
+
+		return value;
 	}
 }
